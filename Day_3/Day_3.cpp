891cc07b57d7331c7c82eb23a6213c0d5e53d7f5
Anton Roth <anton.roth@corpuls.com>
2019-12-04 06:55:27 +0100

    Beautified day 3 by using maps instead of vectors, resulting in slightly less runtime

--- Day_3/Day_3.cpp
+++ Day_3/Day_3.cpp
@@ -7,120 +7,89 @@
 #include <vector>
 #include <sstream>
 #include <string>
+#include <map>
 
 class Wire {
-public:
-	struct Point
-	{
-		int x;
-		int y;
-	};
-
 private:
-	std::vector<Point> passedPoints;
+	std::map<v2, int> passedPointsLength;
 
 public:
 	Wire(std::string path)
 	{
 		std::vector<std::string> directions = split(path, ',');
-		int directionUp, directionLeft;
-		Point start;
-		start.x = 0;
-		start.y = 0;
+        v2 start;
+        int totalSteps = 0;
 
 		for (uint32_t index = 0; index < directions.size(); ++index)
 		{
+            v2 direction;
 			if (directions[index].at(0) == 'L')
-			{
-				directionUp = 0;
-				directionLeft = -1;
-			}
+                direction.x = -1;
+
 			if (directions[index].at(0) == 'R')
-			{
-				directionUp = 0;
-				directionLeft = 1;
-			}
+                direction.x = 1;
+
 			if (directions[index].at(0) == 'U')
-			{
-				directionUp = 1;
-				directionLeft = 0;
-			}
+                direction.y = 1;
+
 			if (directions[index].at(0) == 'D')
-			{
-				directionUp = -1;
-				directionLeft = 0;
-			}
+                direction.y = -1;
 
 			int length = std::stoi(directions[index].substr(1));
 
-			for (int run = 0; run < length; ++run)
+			for (int run = 0; run < length; ++run, ++totalSteps)
 			{
-				start.x += directionLeft;
-				start.y += directionUp;
-
-				passedPoints.push_back(start);
+                start = start + direction;
+                if (passedPointsLength.find(start) == passedPointsLength.end())
+                {
+                    passedPointsLength[start] = totalSteps;
+                }
 			}
 		}
 	}
 
-	int shortestDistance(std::vector<Point>& comparison)
+	int shortestDistance(std::map<v2, int>& comparison)
 	{
 		int minDistance = std::numeric_limits<int>::max();
-		for (uint32_t i = 0; i < comparison.size(); ++i)
-		{
-			for (uint32_t j = 0; j < passedPoints.size(); ++j)
-			{
-				if (passedPoints[j].x == comparison[i].x && passedPoints[j].y == comparison[i].y)
-				{
-					int distance = std::abs(passedPoints[j].x) + std::abs(passedPoints[j].y);
-					if (distance < minDistance)
-					{
-						minDistance = distance;
-					}
-				}
-			}
-		}
+        for (auto elem : comparison)
+        {
+            if (passedPointsLength.find(elem.first) != passedPointsLength.end())
+            {
+                int distance = std::abs(elem.first.x) + std::abs(elem.first.y);
+                if (distance < minDistance)
+                    minDistance = distance;
+            }
+        }
 
 		return minDistance;
 	}
 
-	int shortestSteps(std::vector<Point>& comparison)
+    int shortestSteps(std::map<v2, int>& comparison)
+    {
+        int minDistance = std::numeric_limits<int>::max();
+        for (auto elem : comparison)
+        {
+            if (passedPointsLength.find(elem.first) != passedPointsLength.end())
+            {
+                int distance = elem.second + passedPointsLength[elem.first] + 2;
+                if (distance < minDistance)
+                    minDistance = distance;
+            }
+        }
+
+        return minDistance;
+    }
+
+	std::map<v2, int>& getPoints()
 	{
-		uint32_t minDistance = std::numeric_limits<uint32_t>::max();
-		for (uint32_t i = 0; i < comparison.size(); ++i)
-		{
-			if (i > minDistance)
-				break;
-
-			for (uint32_t j = 0; j < passedPoints.size(); ++j)
-			{
-				if (i + j >= minDistance)
-					break;
-
-				if (passedPoints[j].x == comparison[i].x && passedPoints[j].y == comparison[i].y)
-				{
-					uint32_t distance = i + j + 2;
-					if (distance < minDistance)
-					{
-						minDistance = distance;
-					}
-				}
-			}
-		}
-
-		return minDistance;
-	}
-
-	std::vector<Point>& getPoints()
-	{
-		return passedPoints;
+		return passedPointsLength;
 	}
 };
 
 
 int main()
 {
-	std::vector<std::string> inputs = util::readFileLines("D:\\Development\\Projects\\AdventOfCode\\input_2019_3.txt");
+	std::vector<std::string> inputs = util::readFileLines("..\\input_2019_3.txt");
 	
 	Wire A(inputs[0]);
 	Wire B(inputs[1]);
