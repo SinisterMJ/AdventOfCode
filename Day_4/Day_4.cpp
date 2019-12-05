c60fe0734fa09664a1869ea2445bba3eacccad00
SinisterMJ <Anton_Roth@gmx.de>
2019-12-05 21:10:15 +0100

    Increased speed on day 4 solution massively by jumping to next possible integer which might fulfill criterias.

--- Day_4/Day_4.cpp
+++ Day_4/Day_4.cpp
@@ -28,13 +28,33 @@ std::vector<int> digitsFromInt(int input)
 	return result;
 }
 
-bool validPassword_part1(std::vector<int> digits)
+int intFromDigits(std::vector<int>& input)
+{
+	int result = 0;
+	for (int index = 0; index < input.size(); ++index)
+	{
+		result = result * 10 + input[index];
+	}
+
+	return result;
+}
+
+bool validPassword_part1(std::vector<int> digits, int& externalIndex)
 {
 	bool foundDouble = false;
 	for (int index = 1; index < digits.size(); ++index)
 	{
 		if (digits[index] < digits[index - 1])
+		{
+			//externalIndex += 1 * pow(10, digits.size() - index - 1);
+			for (int i = index; i < digits.size(); ++i)
+			{
+				digits[i] = digits[index - 1];
+			}
+
+			externalIndex = intFromDigits(digits) - 1;
 			return false;
+		}
 
 		if (digits[index] == digits[index - 1])
 			foundDouble = true;
@@ -79,11 +99,11 @@ int main()
 	for (int index = lower; index <= higher; ++index)
 	{
         std::vector<int> input = digitsFromInt(index);
-        if (validPassword_part1(input))
+        if (validPassword_part1(input, index))
         {
             count_part1++;
             count_part2 += validPassword_part2(input);  // Part 2 is a strict subset of part 1
-        }		
+        }
 	}
 
 	std::cout << "Part 1: " << count_part1 << std::endl;
