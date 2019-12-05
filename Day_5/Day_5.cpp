203d7ce5ce53ca6777f6334f020d8d35f439410f
Anton Roth <anton.roth@corpuls.com>
2019-12-05 08:05:07 +0100

    Another clean up code for readability

--- Day_5/Day_5.cpp
+++ Day_5/Day_5.cpp
@@ -30,6 +30,90 @@ std::vector<int> splitInt(const std::string &s, char delim) {
 	return elems;
 }
 
+void analyzeOpcode(int opcode, int modePos1, int modePos2, int modePos3, std::vector<int>& commands, uint32_t& index, int& _input, int& output)
+{
+    if (opcode == operationCodes::opcode_add)
+    {
+        commands[commands[index + 3]] =
+            (modePos1 == 0 ? commands[commands[index + 1]] : commands[index + 1]) +
+            (modePos2 == 0 ? commands[commands[index + 2]] : commands[index + 2]);
+        index += 4;
+    }
+
+    if (opcode == operationCodes::opcode_mul)
+    {
+        commands[commands[index + 3]] =
+            (modePos1 == 0 ? commands[commands[index + 1]] : commands[index + 1]) *
+            (modePos2 == 0 ? commands[commands[index + 2]] : commands[index + 2]);
+        index += 4;
+    }
+
+    if (opcode == operationCodes::opcode_jumptrue)
+    {
+        auto& param_1_val =
+            (modePos1 == 0 ? commands[commands[index + 1]] : commands[index + 1]);
+        if (param_1_val == 0)
+        {
+            index += 3;
+        }
+        else
+        {
+            auto& param_2_val =
+                (modePos2 == 0 ? commands[commands[index + 2]] : commands[index + 2]);
+            index = param_2_val;
+        }
+    }
+
+    if (opcode == operationCodes::opcode_jumpfalse)
+    {
+        auto& param_1_val =
+            (modePos1 == 0 ? commands[commands[index + 1]] : commands[index + 1]);
+        if (param_1_val != 0)
+        {
+            index += 3;
+        }
+        else
+        {
+            auto& param_2_val =
+                (modePos2 == 0 ? commands[commands[index + 2]] : commands[index + 2]);
+            index = param_2_val;
+        }
+    }
+
+    if (opcode == operationCodes::opcode_lessthan)
+    {
+        auto& param_1_val =
+            (modePos1 == 0 ? commands[commands[index + 1]] : commands[index + 1]);
+        auto& param_2_val =
+            (modePos2 == 0 ? commands[commands[index + 2]] : commands[index + 2]);
+        commands[commands[index + 3]] = (param_1_val < param_2_val ? 1 : 0);
+        index += 4;
+    }
+
+    if (opcode == operationCodes::opcode_equals)
+    {
+        auto& param_1_val =
+            (modePos1 == 0 ? commands[commands[index + 1]] : commands[index + 1]);
+        auto& param_2_val =
+            (modePos2 == 0 ? commands[commands[index + 2]] : commands[index + 2]);
+        commands[commands[index + 3]] = (param_1_val == param_2_val ? 1 : 0);
+        index += 4;
+    }
+
+    if (opcode == operationCodes::opcode_input)
+    {
+        commands[commands[index + 1]] = _input;
+        index += 2;
+    }
+
+    if (opcode == operationCodes::opcode_output)
+    {
+        if (commands[commands[index + 1]] != 0)
+            output = commands[commands[index + 1]];
+        index += 2;
+    }
+}
+
 int runCommands(std::vector<int> commands, int _input)
 {
     std::vector<int> outputs;
@@ -40,87 +124,11 @@ int runCommands(std::vector<int> commands, int _input)
 		int param_1 = (commands[index] / 100) % 10;
 		int param_2 = (commands[index] / 1000) % 10;
 		int param_3 = (commands[index] / 10000) % 10;
-        
-        if (opcode == operationCodes::opcode_add)
-        {
-            commands[commands[index + 3]] =
-                (param_1 == 0 ? commands[commands[index + 1]] : commands[index + 1]) +
-                (param_2 == 0 ? commands[commands[index + 2]] : commands[index + 2]);
-            index += 4;
-        }
-        if (opcode == operationCodes::opcode_mul)
-        {
-            commands[commands[index + 3]] =
-                (param_1 == 0 ? commands[commands[index + 1]] : commands[index + 1]) *
-                (param_2 == 0 ? commands[commands[index + 2]] : commands[index + 2]);
-            index += 4;
-        }
-        if (opcode == operationCodes::opcode_input)
-        {
-            commands[commands[index + 1]] = _input;
-            index += 2;
-        }
-        if (opcode == operationCodes::opcode_output)
-        {
-            if (commands[commands[index + 1]] != 0)
-            {
-                output = commands[commands[index + 1]];
-            }
-            index += 2;
-        }
-        if (opcode == operationCodes::opcode_jumptrue)
-        {
-            auto& param_1_val =
-                (param_1 == 0 ? commands[commands[index + 1]] : commands[index + 1]);
-            if (param_1_val == 0)
-            {
-                index += 3;
-            }
-            else
-            {
-                auto& param_2_val =
-                    (param_2 == 0 ? commands[commands[index + 2]] : commands[index + 2]);
-                index = param_2_val;
-            }
-        }
-        if (opcode == operationCodes::opcode_jumpfalse)
-        {
-            auto& param_1_val =
-                (param_1 == 0 ? commands[commands[index + 1]] : commands[index + 1]);
-            if (param_1_val != 0)
-            {
-                index += 3;
-            }
-            else
-            {
-                auto& param_2_val =
-                    (param_2 == 0 ? commands[commands[index + 2]] : commands[index + 2]);
-                index = param_2_val;
-            }
-        }
-        if (opcode == operationCodes::opcode_lessthan)
-        {
-            auto& param_1_val =
-                (param_1 == 0 ? commands[commands[index + 1]] : commands[index + 1]);
-            auto& param_2_val =
-                (param_2 == 0 ? commands[commands[index + 2]] : commands[index + 2]);
-            commands[commands[index + 3]] = (param_1_val < param_2_val ? 1 : 0);
-            index += 4;
-        }
-        if (opcode == operationCodes::opcode_equals)
-        {
-            auto& param_1_val =
-                (param_1 == 0 ? commands[commands[index + 1]] : commands[index + 1]);
-            auto& param_2_val =
-                (param_2 == 0 ? commands[commands[index + 2]] : commands[index + 2]);
-            commands[commands[index + 3]] = (param_1_val == param_2_val ? 1 : 0);
-            index += 4;
-        }
+
+        analyzeOpcode(opcode, param_1, param_2, param_3, commands, index, _input, output);
 
         if (opcode == operationCodes::opcode_terminate)
-        {
             return output;
-        }
 	}
 
 	return -1;
