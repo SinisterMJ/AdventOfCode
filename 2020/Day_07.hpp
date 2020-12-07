211538c5f919064b271b071fc9d3c7c55f4e3c03
SinisterMJ <Anton_Roth@gmx.de>
2020-12-07 07:17:30 +0100

    Beautified the code.

--- 2020/Day_07.hpp
+++ 2020/Day_07.hpp
@@ -18,34 +18,27 @@ private:
     std::map<std::string, std::vector<std::pair<int, std::string>>> allBagsMap;
 
     void ReadBags() {
+        std::regex bag_regex("([0-9]+) (.*) bag");
+        std::smatch bag_match;
+
         for (auto elem : inputVec)
         {
-            std::string outer = elem.substr(0, elem.find("contain"));
-            bags single;
-            single.outer = outer.substr(0, outer.find("bag") - 1);
-
+            std::string outer = elem.substr(0, elem.find("bag") - 1);
             std::string inner = elem.substr(elem.find("contain") + 8);
-
+            
             if (inner == "no other bags.")
-            {
-                allBagsMap[single.outer] = std::vector<std::pair<int, std::string>>();
                 continue;
-            }
-
+            
             auto innerList = util::split(inner, ',');
 
             for (auto innerElem : innerList)
             {
-                std::regex bag_regex("([0-9]+) (.*) bag");
-                std::smatch bag_match;
                 std::regex_search(innerElem, bag_match, bag_regex);
                 int count = std::stoi(bag_match[1]);
                 std::string color = bag_match[2];
 
-                single.inner.push_back(std::pair<int, std::string>(count, color));
+                allBagsMap[outer].push_back(std::pair<int, std::string>(count, color));
             }
-
-            allBagsMap[single.outer] = single.inner;
         }
     }
 
@@ -71,10 +64,8 @@ private:
         int result = 0;
 
         for (auto elem : allBagsMap)
-        {
             result += containsShiny(elem.first);
-        }
-
+        
         return result;
     }
 
@@ -84,10 +75,8 @@ private:
         auto list = allBagsMap[outer];
 
         for (auto elem : list)
-        {
             result += elem.first * totalNumberBags(elem.second);
-        }
-
+        
         return result;
     }
 
@@ -104,11 +93,11 @@ public:
 		myTime.start();
         ReadBags();
 
-        int32_t result_1 = numberBags();
-        int32_t result_2 = totalNumberBags("shiny gold") - 1;  // shiny gold doesn't count
+        //int32_t result_1 = numberBags();
+        //int32_t result_2 = totalNumberBags("shiny gold") - 1;  // shiny gold doesn't count
 
-        std::cout << "Day 07 - Part 1: " << result_1 << '\n'
-                  << "Day 07 - Part 2: " << result_2 << '\n';
+        //std::cout << "Day 07 - Part 1: " << result_1 << '\n'
+          //        << "Day 07 - Part 2: " << result_2 << '\n';
 
 		return myTime.usPassed();
 	}
