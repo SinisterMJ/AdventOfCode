49fbed3149f87c69d8c95201a102f1c0230731f1
SinisterMJ <Anton_Roth@gmx.de>
2020-12-22 20:18:21 +0100

    Optimized Day 22 solution.

--- 2020/Day_22.hpp
+++ 2020/Day_22.hpp
@@ -2,74 +2,86 @@
 #define ADVENTOFCODE2020_DAY22
 
 #include "../includes/aoc.h"
-#include <queue>
+#include <unordered_set>
+#include <deque>
 
-class Day22 
+class Day22
 {
 private:
-    int64_t part1() 
+    int64_t part1()
     {
         int64_t result = 0;
-        std::queue<int64_t> deck_1;
-        std::queue<int64_t> deck_2;
+        std::deque<int64_t> deck_1;
+        std::deque<int64_t> deck_2;
 
         for (auto elem : player_1)
         {
-            deck_1.push(elem);
+            deck_1.push_back(elem);
         }
 
         for (auto elem : player_2)
         {
-            deck_2.push(elem);
+            deck_2.push_back(elem);
         }
 
         while (deck_1.size() > 0 && deck_2.size() > 0)
         {
-            int64_t val_1 = deck_1.front(); deck_1.pop();
-            int64_t val_2 = deck_2.front(); deck_2.pop();
+            int64_t val_1 = deck_1.front(); deck_1.pop_front();
+            int64_t val_2 = deck_2.front(); deck_2.pop_front();
 
             if (val_1 > val_2)
             {
-                deck_1.push(val_1);
-                deck_1.push(val_2);
+                deck_1.push_back(val_1);
+                deck_1.push_back(val_2);
             }
 
             if (val_1 < val_2)
             {
-                deck_2.push(val_2);
-                deck_2.push(val_1);
+                deck_2.push_back(val_2);
+                deck_2.push_back(val_1);
             }
         }
 
-        std::queue<int64_t> finalDeck;
+        std::deque<int64_t> finalDeck;
 
         if (deck_1.size() > 0)
             finalDeck = deck_1;
         else
             finalDeck = deck_2;
 
-        for ( ; finalDeck.size() > 0; )
+        for (; finalDeck.size() > 0; )
         {
             result += finalDeck.size() * finalDeck.front();
-            finalDeck.pop();
+            finalDeck.pop_front();
         }
 
         return result;
     }
 
+    int64_t getHistory(std::deque<int64_t>& deck)
+    {
+        int64_t hist = 0;
+        for (int index = 0; index < deck.size(); ++index)
+            hist += primes[index] * deck[index];
+
+        return hist;
+    }
+
     bool playRound(std::deque<int64_t>& deck_1, std::deque<int64_t>& deck_2)
     {
-        std::vector< std::pair<std::deque<int64_t>, std::deque<int64_t>> > history;
-
+        std::set< std::pair<int64_t, int64_t> > history_int;
         while (deck_1.size() > 0 && deck_2.size() > 0)
         {
-            auto currentHistory = std::make_pair(deck_1, deck_2);
-
+            int64_t hist_1 = getHistory(deck_1);
+            int64_t hist_2 = getHistory(deck_2);
+            
+            auto currentHist_int = std::make_pair(hist_1, hist_2);
+            
             // Rule 1 of recursive combat
-            if (std::find(history.begin(), history.end(), currentHistory) != history.end())
+            if (history_int.find(currentHist_int) != history_int.end())
                 return true;
 
-            history.push_back(currentHistory);
+            history_int.insert(currentHist_int);
 
             int64_t val_1 = deck_1.front();
             int64_t val_2 = deck_2.front();
@@ -141,6 +153,8 @@ private:
     std::vector<int64_t> player_1;
     std::vector<int64_t> player_2;
 
+    std::vector<int64_t> primes{ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 
+        127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229 };
     std::string inputString;
     std::vector<std::string> inputVec;
 public:
