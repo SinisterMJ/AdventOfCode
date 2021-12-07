15d2bfdf4256c4791ed40fc909f87cdb279a0d0e
SinisterMJ <Anton_Roth@gmx.de>
2021-12-07 06:37:14 +0100

    Day 7 - 2021

--- 2017/Day_11.hpp
+++ 2017/Day_11.hpp
@@ -1,122 +1,39 @@
 #ifndef ADVENTOFCODE2017_DAY11
 #define ADVENTOFCODE2017_DAY11
 
-#include <map>
-
 #include "../includes/aoc.h"
-#include "../includes/IntcodeVM.h"
-#include "../includes/Map2DBase.h"
 
 class Day11 {
 private:
-	std::string inputString;
+    std::string inputString;
+    std::vector<std::string> inputVec;
+    
+    std::vector<v2> neighbours{ v2(1, 0), v2(1, 1), v2(0, 1), v2(-1, 0), v2(-1, -1), v2(0, -1) };
+    enum Direction {
+        east,
+        southeast,
+        southwest,
+        west,
+        northwest,
+        northeast
+    };
 
-    std::string increment(std::string input)
+    int64_t part1()
     {
-        std::string result = input;
-
-        // Shortcut for i o l
-        for (int8_t index = 0; index < 8; ++index)
-        {
-            if (result[index] == 'i' || result[index] == 'o' || result[index] == 'l')
-            {
-                result[index]++;
-                for (int8_t subIdx = index + 1; subIdx < 8; ++subIdx)
-                {
-                    result[subIdx] = 'a';
-                }
-
-                return result;
-            }
-        }
-        
-        for (int8_t index = 7; index >= 0; --index)
-        {
-            if (result[index] == 'z')
-            {
-                result[index] = 'a';
-            }
-            else
-            {
-                result[index]++;
-                break;
-            }
-        }
-
-        return result;
+        return 0;
     }
 
-    bool checkValid(std::string password)
+    int64_t part2()
     {
-        // Rule 1
-        bool foundTriple = false;
-        for (int8_t index = 0; index < 6 && !foundTriple; ++index)
-        {
-            auto a = password[index + 0];
-            auto b = password[index + 1];
-            auto c = password[index + 2];
-
-            if ((a + 1 == b) && (b + 1 == c))
-                foundTriple = true;
-        }
-
-        if (!foundTriple)
-            return false;
-
-        // Rule 2
-        if (password.find('i') != std::string::npos ||
-            password.find('o') != std::string::npos ||
-            password.find('l') != std::string::npos)
-            return false;
-
-        // Rule 3
-        bool foundFirstDouble = false;
-        int8_t firstDouble = '0';
-        for (int8_t index = 0; index < 7; ++index)
-        {
-            if (password[index] == password[index + 1] && password[index] != firstDouble)
-            {
-                if (foundFirstDouble)
-                    return true;
-
-                foundFirstDouble = true;
-                firstDouble = password[index];
-                index++;
-            }
-        }
-
-        return false;
-    }
-
-    std::string part1()
-    {
-        std::string current = increment(inputString);
-
-        while (!checkValid(current))
-        {
-            current = increment(current);
-        }
-
-        return current;
-    }
-
-    std::string part2(std::string start)
-    {
-        std::string current = increment(start);
-
-        while (!checkValid(current))
-        {
-            current = increment(current);
-        }
-
-        return current;
+        return 0;
     }
 
 public:
-	Day11()
-	{
-		inputString = util::readFile("..\\inputs\\2017\\input_11.txt");
-	}
+    Day11()
+    {
+        inputString = util::readFile("..\\inputs\\2017\\input_11.txt");
+        inputVec = util::readFileLines("..\\inputs\\2017\\input_11.txt", '\n', true);
+    }
 
     int64_t run()
     {
@@ -124,12 +41,12 @@ public:
         myTime.start();
 
         auto result_1 = part1();
-        auto result_2 = part2(result_1);
-        
-        int64_t time = myTime.usPassed();
+        auto result_2 = part2();
 
-        std::cout << "Day 11 - Part 1: " << result_1 << '\n'
-                  << "Day 11 - Part 2: " << result_2 << '\n';
+        int64_t time = myTime.usPassed();
+        std::cout 
+            << "Day 11 - Part 1: " << result_1 << '\n'
+            << "Day 11 - Part 2: " << result_2 << '\n';
 
         return time;
     }
--- 2021/Day_07.hpp
+++ 2021/Day_07.hpp
@@ -7,13 +7,42 @@ class Day07 {
 private:
 
     std::string inputString;
-    std::vector<std::string> inputVector;
+    std::vector<int32_t> positions;
+
+    int solve(bool incremental)
+    {
+        int fuel = 0;
+        int last_fuel = std::numeric_limits<int>::max();
+        for (int i = 0;; ++i)
+        { 
+            fuel = 0;
+
+            for (auto& pos : positions)
+            {
+                if (incremental)
+                {
+                    auto this_fuel = std::abs(pos - i);
+                    fuel += (this_fuel * (this_fuel + 1)) / 2;
+                }
+                else
+                {
+                    fuel += std::abs(pos - i);
+                }                
+            }
+
+            if (last_fuel < fuel)
+                return last_fuel;
+
+            last_fuel = fuel;
+        }
+
+        return 0;
+    }
 
 public:
     Day07()
     {
         inputString = util::readFile("..\\inputs\\2021\\input_7.txt");
-        inputVector = util::readFileLines("..\\inputs\\2021\\input_7.txt");
     }
 
     int64_t run()
@@ -21,8 +50,11 @@ public:
         util::Timer myTime;
         myTime.start();
 
-        int32_t result_1 = 0;
-        int32_t result_2 = 0;
+        auto s_pos = util::split(inputString, ',');
+        positions = util::ConvertToInt(s_pos);
+
+        int32_t result_1 = solve(false);
+        int32_t result_2 = solve(true);
 
         int64_t time = myTime.usPassed();
 
