91d3977f4b8c1bf24d278898d163eb7bbbe2fe44
SinisterMJ <Anton_Roth@gmx.de>
2021-12-11 06:25:10 +0100

    Day 11, 2021 finished

--- 2021/Day_11.hpp
+++ 2021/Day_11.hpp
@@ -7,16 +7,85 @@ class Day11 {
 private:
     std::string inputString;
     std::vector<std::string> inputVec;
-    std::vector<int64_t> adapters;
+    std::vector<v2> neighbours = MapHelper::getNeighboursVec(true);
 
-    int64_t part1()
+    std::pair<int32_t, int32_t> solve()
     {
-        return 0;
-    }
+        std::map<v2, int> octos;
+        v2 curr_pos(0, 0);
 
-    int64_t part2()
-    {
-        return 0;
+        for (auto line : inputVec)
+        {
+            curr_pos.y = 0;
+            for (auto ch : line)
+            {
+                int d = ch - '0';
+
+                octos[curr_pos] = d;
+                curr_pos.y++;
+            }
+            curr_pos.x++;
+        }
+
+        int result = 0;
+        int result_inner = 0;
+
+        for (int i = 1;; ++i)
+        {
+            std::map<v2, int> octo_new;
+
+            if (i == 101)
+                result = result_inner;
+
+            for (auto [pos, charge] : octos)
+            {
+                octos[pos] = charge + 1;
+            }
+
+            while (octos.size() != octo_new.size())
+            {
+                bool find_max = false;
+                for (auto [pos, charge] : octos)
+                {
+                    if (charge >= 10)
+                    {
+                        result_inner++;
+                        octo_new[pos] = 0;
+                        octos[pos] = 0;
+                        find_max = true;
+
+                        for (auto n : neighbours)
+                        {
+                            if (octos.find(n + pos) != octos.end() && octos[n + pos] != 0)
+                            {
+                                octos[n + pos] = octos[n + pos] + 1;
+                            }
+                        }
+                    }
+                }
+
+                if (!find_max)
+                {
+                    for (auto [pos, charge] : octos)
+                    {
+                        if (octo_new.find(pos) == octo_new.end())
+                            octo_new[pos] = charge;
+                    }
+                }
+            }
+
+            std::swap(octos, octo_new);
+
+            int sum = 0;
+
+            for (auto [pos, charge] : octos)
+                sum += charge;
+
+            if (sum == 0)
+                return std::make_pair(result, i);
+        }
+
+        return std::make_pair(0, 0);
     }
 
 public:
@@ -31,10 +100,9 @@ public:
         util::Timer myTime;
         myTime.start();
 
-        adapters = util::ConvertToInt64(inputVec);
-
-        auto result_1 = part1();
-        auto result_2 = part2();
+        auto result = solve();
+        auto result_1 = result.first;
+        auto result_2 = result.second;
 
         int64_t time = myTime.usPassed();
         std::cout << "Day 11 - Part 1: " << result_1 << '\n'
