0744bfecca068d783aa14f0e7632f22077f96aad
SinisterMJ <Anton_Roth@gmx.de>
2021-12-13 06:35:49 +0100

    Day 13 2021. Really slow right now due to the many map look ups. Will rewrite it to use a vector or something.

--- 2021/AdventOfCode_2021.cpp
+++ 2021/AdventOfCode_2021.cpp
@@ -89,19 +89,19 @@ int main()
     {
         Day11 mDay11; runTimes.emplace_back(mDay11.run());
         std::cout << std::endl;
-    }*/
+    }
 
     {
         Day12 mDay12; runTimes.emplace_back(mDay12.run());
         std::cout << std::endl;
-    }
+    }*/
 
-    /*{
+    {
         Day13 mDay13; runTimes.emplace_back(mDay13.run());
         std::cout << std::endl;
     }
 
-    {
+    /*{
         Day14 mDay14; runTimes.emplace_back(mDay14.run());
         std::cout << std::endl;
     }
--- 2021/Day_13.hpp
+++ 2021/Day_13.hpp
@@ -3,20 +3,156 @@
 
 #include "../includes/aoc.h"
 
+
 class Day13 {
 private:
     std::string inputString;
     std::vector<std::string> inputVec;
-    std::vector<int64_t> adapters;
+    std::map<v2, int8_t> dots;
+    std::vector<std::string> folds;
+
+    std::map<v2, int8_t> fold_map(std::map<v2, int8_t> input, std::string task)
+    {
+        auto axis = task.substr(0, 1);
+        auto position = std::stoi(task.substr(2));
+
+        std::map<v2, int8_t> result;
+
+        auto max_x = 0;
+        auto max_y = 0;
+
+        for (auto [pos, val] : input)
+        {
+            max_x = std::max(max_x, pos.x);
+            max_y = std::max(max_y, pos.y);
+        }
+
+        if (axis == "x")
+        {
+            for (auto ind = position - 1; ind >= 0; --ind)
+            {
+                for (auto y = 0; y <= max_y; ++y)
+                {
+                    v2 pos = v2(ind, y);
+                    result[pos] = input[pos];
+                }
+            }
+
+            for (auto offset = 1; offset + position <= max_x; ++offset)
+            {
+                for (auto y = 0; y <= max_y; ++y)
+                {
+                    v2 pos = v2(offset + position, y);
+                    if (input[pos] == '#')
+                        result[v2(position - offset, y)] = input[pos];
+                }
+            }
+        }
+
+        if (axis == "y")
+        {
+            for (auto ind = position - 1; ind >= 0; --ind)
+            {
+                for (auto x = 0; x <= max_x; ++x)
+                {
+                    v2 pos = v2(x, ind);
+                    result[pos] = input[pos];
+                }
+            }
+
+            for (auto offset = 1; offset + position <= max_y; ++offset)
+            {
+                for (auto x = 0; x <= max_x; ++x)
+                {
+                    v2 pos = v2(x, offset + position);
+                    if (input[pos] == '#')
+                        result[v2(x, position - offset)] = input[pos];
+                }
+            }
+        }
+
+        return result;
+    }
 
     int64_t part1()
     {
-        return 0;
+
+        for (auto ln : inputVec)
+        {
+            if (ln.find("fold") != std::string::npos)
+            {
+                folds.push_back(ln);
+            }
+            else
+            {
+                auto positions = util::splitInt(ln, ',');
+                dots[v2(positions[0], positions[1])] = '#';
+            }
+        }
+
+        std::string instruction = folds[0].substr(std::string("fold along ").size());
+
+        auto result = fold_map(dots, instruction);
+
+        int sum = 0;
+        for (auto [pos, val] : result)
+        {
+            if (val == '#')
+                sum++;
+        }
+
+        return sum;
     }
 
-    int64_t part2()
+    std::string part2()
     {
-        return 0;
+        std::vector<std::string> folds;
+
+        for (auto ln : inputVec)
+        {
+            if (ln.find("fold") != std::string::npos)
+            {
+                folds.push_back(ln);
+            }
+            else
+            {
+                auto positions = util::splitInt(ln, ',');
+                dots[v2(positions[0], positions[1])] = '#';
+            }
+        }
+
+        std::string result = "";
+        std::map<v2, int8_t> input = dots;
+
+        for (auto ln : folds)
+        {
+            std::string instruction = ln.substr(std::string("fold along ").size());
+            input = fold_map(input, instruction);
+        }
+
+        int max_x = 0;
+        int max_y = 0;
+
+        for (auto [pos, val] : input)
+        {
+            max_x = std::max(max_x, pos.x);
+            max_y = std::max(max_y, pos.y);
+        }
+
+        for (int y = 0; y <= max_y; ++y)
+        {
+            result += "\n";
+
+            for (int x = 0; x <= max_x; ++x)
+            {
+                if (input[v2(x, y)] == 0)
+                    result += " ";
+                else
+                    result += "#";
+            }
+        }
+
+        return result;
     }
 
 public:
@@ -30,8 +166,6 @@ public:
     {
         util::Timer myTime;
         myTime.start();
-
-        adapters = util::ConvertToInt64(inputVec);
 
         auto result_1 = part1();
         auto result_2 = part2();
