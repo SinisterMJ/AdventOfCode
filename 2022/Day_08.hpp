36a5686f7110d52301911173c176391ee54613a0
Anton Roth <Anton.Roth@corpuls.com>
2022-12-08 06:27:01 +0100

    Day 8 - 2022

--- 2022/Day_08.hpp
+++ 2022/Day_08.hpp
@@ -9,6 +9,153 @@ private:
     std::vector<std::string> inputVector;
     std::string inputString;
 
+    std::map<v2, int8_t> trees;
+    std::map<v2, bool> not_seen;
+
+    int part1()
+    {
+        for (int y = 0; y < inputVector.size(); ++y)
+        {
+            for (int x = 0; x < inputVector[y].size(); ++x)
+            {
+                v2 pos(x, y);
+                trees[pos] = inputVector[y][x] - '0';
+                not_seen[pos] = true;
+            }
+        }
+
+        int64_t sum = 0;
+
+        for (int y = 0; y < inputVector.size(); ++y)
+        {
+            int8_t lastHeight = 0;
+            for (int x = 0; x < inputVector[y].size(); ++x)
+            {
+                v2 pos(x, y);
+                if (trees[pos] > lastHeight)
+                {
+                    lastHeight = trees[pos];
+                    sum += not_seen[pos];
+                    not_seen[pos] = false;
+                }
+            }
+
+            lastHeight = 0;
+            for (int x = inputVector[y].size() - 1 ; x >= 0; --x)
+            {
+                v2 pos(x, y);
+                if (trees[pos] > lastHeight)
+                {
+                    lastHeight = trees[pos];
+                    sum += not_seen[pos];
+                    not_seen[pos] = false;
+                }
+            }
+        }
+
+        for (int x = 0; x < inputVector[0].size(); ++x)
+        {
+            int8_t lastHeight = 0;
+            for (int y = 0; y < inputVector.size(); ++y)
+            {
+                v2 pos(x, y);
+                if (trees[pos] > lastHeight)
+                {
+                    lastHeight = trees[pos];
+                    sum += not_seen[pos];
+                    not_seen[pos] = false;
+                }
+            }
+            lastHeight = 0;
+            for (int y = inputVector.size() - 1; y >= 0; --y)
+            {
+                v2 pos(x, y);
+                if (trees[pos] > lastHeight)
+                {
+                    lastHeight = trees[pos];
+                    sum += not_seen[pos];
+                    not_seen[pos] = false;
+                }
+            }
+        }
+
+        return sum;
+    }
+    
+    int part2()
+    {
+        int64_t best = 0;
+
+        for (int y = 0; y < inputVector.size(); ++y)
+        {
+            for (int x = 0; x < inputVector[y].size(); ++x)
+            {
+                int32_t view_up = 0, view_down = 0, view_left = 0, view_right = 0;
+                v2 currPos(x, y);
+                int8_t curr_height = trees[v2(x, y)];
+
+                if (currPos == v2(2, 3))
+                    int breakHere = 0;
+
+                for (view_up = 0; y - view_up >= 0; ++view_up)
+                {
+                    v2 pos(x, y - view_up);
+                    if (currPos == pos)
+                        continue;
+                    if (trees[pos] >= curr_height)
+                    {
+                        view_up++;
+                        break;
+                    }
+                }
+
+                view_up--;
+
+                for (view_down = 0; y + view_down < inputVector.size(); ++view_down)
+                {
+                    v2 pos(x, y + view_down);
+                    if (currPos == pos)
+                        continue;
+                    if (trees[pos] >= curr_height)
+                    {
+                        view_down++;
+                        break;
+                    }
+                }
+                view_down--;
+
+                for (view_left = 0; x - view_left >= 0; ++view_left)
+                {
+                    v2 pos(x - view_left, y);
+                    if (currPos == pos)
+                        continue;
+                    if (trees[pos] >= curr_height)
+                    {
+                        view_left++;
+                        break;
+                    }
+                }
+
+                view_left--;
+
+                for (view_right = 0; x + view_right < inputVector[0].size(); ++view_right)
+                {
+                    v2 pos(x + view_right, y);
+                    if (currPos == pos)
+                        continue;
+                    if (trees[pos] >= curr_height)
+                    {
+                        view_right++;
+                        break;
+                    }
+                }
+                view_right--;
+
+                best = std::max<int64_t>(best, view_down * view_up * view_left * view_right);
+            }
+        }
+        return best;
+    }
 public:
     Day08()
     {
@@ -21,8 +168,8 @@ public:
         util::Timer myTime;
         myTime.start();
 
-        auto result_1 = 0;
-        auto result_2 = 0;
+        auto result_1 = part1();
+        auto result_2 = part2();
 
         int64_t time = myTime.usPassed();
 
