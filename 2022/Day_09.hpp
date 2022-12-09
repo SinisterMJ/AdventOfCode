f722646c7fe62e7293ba27e0a39c436d9b9e46c2
Anton Roth <Anton.Roth@corpuls.com>
2022-12-09 06:35:10 +0100

    Day 9 - 2022

--- 2022/Day_09.hpp
+++ 2022/Day_09.hpp
@@ -9,14 +9,91 @@ private:
     std::vector<std::string> inputVector;
     std::string inputString;
 
-    int part1()
+    v2 new_position(v2 head, v2 curr)
     {
-        return 0;
+        // They are touching
+        if (std::abs(head.x - curr.x) <= 1 && std::abs(head.y - curr.y) <= 1)
+            return curr;
+
+        if (std::abs(head.x - curr.x) > 0)
+            curr.x += sgn(head.x - curr.x);
+
+        if (std::abs(head.y - curr.y) > 0)
+            curr.y += sgn(head.y - curr.y);
+
+        return curr;
     }
 
-    int part2()
+    int64_t part1()
     {
-        return 0;
+        v2 head(0, 0);
+        v2 tail(0, 0);
+        std::set<v2> tailPositions;
+        tailPositions.insert(tail);
+
+        for (auto line : inputVector)
+        {
+            auto moves = util::split(line, ' ');
+
+            v2 dir(0, 0);
+
+            if (moves[0] == "R")
+                dir = v2(1, 0);
+            if (moves[0] == "L")
+                dir = v2(-1, 0);
+            if (moves[0] == "U")
+                dir = v2(0, 1);
+            if (moves[0] == "D")
+                dir = v2(0, -1);
+
+            for (int index = 0; index < std::stoi(moves[1]); ++index)
+            {
+                head += dir;
+                tail = new_position(head, tail);
+                tailPositions.insert(tail);
+            }
+        }
+
+        return tailPositions.size();
+    }
+
+
+    int64_t part2()
+    {
+        std::array<v2, 10> tails;
+        
+        std::set<v2> tailPositions;
+        tailPositions.insert(v2(0, 0));
+
+        for (auto line : inputVector)
+        {
+            auto moves = util::split(line, ' ');
+
+            v2 dir(0, 0);
+
+            if (moves[0] == "R")
+                dir = v2(1, 0);
+            if (moves[0] == "L")
+                dir = v2(-1, 0);
+            if (moves[0] == "U")
+                dir = v2(0, 1);
+            if (moves[0] == "D")
+                dir = v2(0, -1);
+
+            for (int index = 0; index < std::stoi(moves[1]); ++index)
+            {
+                tails[0] += dir;
+
+                for (int i = 1; i < 10; ++i)
+                {
+                    tails[i] = new_position(tails[i - 1], tails[i]);
+                }
+
+                tailPositions.insert(tails[9]);
+            }
+        }
+
+        return tailPositions.size();
     }
 public:
     Day09()
