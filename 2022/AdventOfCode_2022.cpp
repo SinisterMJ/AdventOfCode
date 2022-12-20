382a432f40a771d81a67418c597b2e7a9663b378
SinisterMJ <Anton_Roth@gmx.de>
2022-12-20 09:27:00 +0100

    Day 20 - 2022

--- 2022/AdventOfCode_2022.cpp
+++ 2022/AdventOfCode_2022.cpp
@@ -124,19 +124,19 @@ int main()
     {
         Day18 mDay18; runTimes.emplace_back(mDay18.run());
         std::cout << std::endl;
-    }*/
+    }
 
     {
         Day19 mDay19; runTimes.emplace_back(mDay19.run());
         std::cout << std::endl;
-    }
+    }*/
 
-    /*{
+    {
         Day20 mDay20; runTimes.emplace_back(mDay20.run());
         std::cout << std::endl;
     }
 
-    {
+    /*{
         Day21 mDay21; runTimes.emplace_back(mDay21.run());
         std::cout << std::endl;
     }
--- 2022/Day_20.hpp
+++ 2022/Day_20.hpp
@@ -9,14 +9,60 @@ private:
     std::vector<std::string> inputVector;
     std::string inputString;
 
-    int part1()
+    int64_t solve(int64_t _factor, int64_t loop_count)
     {
-        return 0;
-    }
+        auto numbers_orig = util::ConvertToInt(inputVector);
+        std::vector<std::pair<int64_t, int64_t>> numbers;
+        numbers.reserve(numbers_orig.size());
+        int64_t factor = _factor;
 
-    int part2()
-    {
-        return 0;
+        for (int i = 0; i < numbers_orig.size(); ++i)
+            numbers.push_back(std::make_pair(numbers_orig[i] * factor, i));
+
+        for (int loops = 0; loops < loop_count; ++loops)
+        {
+            for (int index = 0; index < numbers.size(); ++index)
+            {
+                // Find it
+                int64_t index_val = 0;
+                for (int i = 0; i < numbers.size(); ++i)
+                {
+                    if (numbers[i].second == index)
+                    {
+                        index_val = i;
+                        break;
+                    }
+                }
+
+                int64_t val = numbers[index_val].first;
+                numbers.erase(numbers.begin() + index_val);
+
+                int64_t offset = index_val + val;
+                int64_t div = (numbers_orig.size() - 1);
+                int64_t temp = std::abs(offset / div);
+                if (offset < 0)
+                {
+                    offset += (temp + 1) * (numbers_orig.size() - 1);
+                }
+
+                offset %= (numbers_orig.size() - 1);
+                numbers.insert(numbers.begin() + offset, std::make_pair(val, index));
+            }
+        }
+
+        int64_t val_1, val_2, val_3;
+
+        int index_0 = 0;
+
+        for (int index = 0; index < numbers.size(); ++index)
+            if (numbers[index].first == 0)
+                index_0 = index;
+
+        val_1 = numbers[(index_0 + 1000) % numbers.size()].first;
+        val_2 = numbers[(index_0 + 2000) % numbers.size()].first;
+        val_3 = numbers[(index_0 + 3000) % numbers.size()].first;
+
+        return val_1 + val_2 + val_3;
     }
 
 public:
@@ -31,8 +77,8 @@ public:
         util::Timer myTime;
         myTime.start();
 
-        auto result_1 = part1();
-        auto result_2 = part2();
+        auto result_1 = solve(1, 1);
+        auto result_2 = solve(811589153, 10);
 
         int64_t time = myTime.usPassed();
 
