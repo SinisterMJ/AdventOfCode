f91133eb147aba6dc36b4e8fb8487eeb92aa3a6a
Anton Roth <Anton.Roth@corpuls.com>
2023-12-07 20:59:03 +0100

    Beautified code Day 7. Runtime optimized by using std::sort instead of bubble sort

--- 2023/Day_07.hpp
+++ 2023/Day_07.hpp
@@ -9,226 +9,144 @@ private:
 
     std::vector<std::string> inputVector;
     std::string inputString;
+    std::map<int8_t, int8_t> cardStrength;
     
     struct CamelCards {
         std::string cards;
         int bid;
+        int8_t rank;
 
-        std::map<int8_t, int8_t> cardStrength;
 
         CamelCards() {
             bid = 0;
+            rank = 0;
             cards = "";
-            cardStrength['2'] = 0;
-            cardStrength['3'] = 1;
-            cardStrength['4'] = 2;
-            cardStrength['5'] = 3;
-            cardStrength['6'] = 4;
-            cardStrength['7'] = 5;
-            cardStrength['8'] = 6;
-            cardStrength['9'] = 7;
-            cardStrength['T'] = 8;
-            cardStrength['J'] = 9;
-            cardStrength['Q'] = 10;
-            cardStrength['K'] = 11;
-            cardStrength['A'] = 12;
         }
 
-        int rankLevel(std::map<int8_t, int8_t>& dict)
-        {
-            for (auto [ch, occ] : dict)
-            {
-                if (occ == 5)
-                    return 7;
-                if (occ == 4)
-                    return 6;
-
-                if (occ == 3)
-                {
-                    for (auto [ch_i, occ_i] : dict)
-                    {
-                        if (occ_i == 2)
-                            return 5;
-                    }
-                    return 4;
-                }
-
-                if (occ == 2)
-                {
-                    for (auto [ch_i, occ_i] : dict)
-                    {
-                        if (ch_i != ch && occ_i == 2)
-                            return 3;
-                        if (occ_i == 3)
-                            return 5;
-                    }
-
-                    return 2;
-                }
-            }
-
-            return 1;
-        }
-
-        bool bigger(CamelCards comp)
+        int8_t rankLevel(bool part_2)
         {
             std::map<int8_t, int8_t> occurences;
-            std::map<int8_t, int8_t> occurences_comp;
+            std::vector<int8_t> occs;
 
             for (auto ch : cards)
                 occurences[ch] += 1;
 
-            for (auto ch : comp.cards)
-                occurences_comp[ch] += 1;
-
-            auto rank = rankLevel(occurences);
-            auto rank_comp = rankLevel(occurences_comp);
-
-            if (rank == rank_comp)
-            {
-                for (int index = 0; index < cards.size(); ++index)
-                {
-                    if (cards[index] != comp.cards[index])
-                    {
-                        return cardStrength[cards[index]] > cardStrength[comp.cards[index]];
-                    }
-                }
-
-                return false;
-            }
-
-            return rank > rank_comp;
-        }
-
-        bool bigger_part2(CamelCards comp)
-        {
-            std::map<int8_t, int8_t> occurences;
-            std::map<int8_t, int8_t> occurences_comp;
-            cardStrength['J'] = -1;
-
-            for (auto ch : cards)
-                occurences[ch] += 1;
-
-            for (auto ch : comp.cards)
-                occurences_comp[ch] += 1;
-
-            int8_t max = 0;
-            for (auto [ch, occ] : occurences)
-            {
-                if (ch == 'J')
-                    continue;
-                max = std::max(occ, max);
-            }
+            int8_t num_j = 0;
 
             for (auto [ch, occ] : occurences)
             {
-                if (ch == 'J')
-                    continue;
-                if (occ == max)
+                if (part_2 && ch == 'J')
                 {
-                    occurences[ch] += occurences['J'];
-                    occurences['J'] = 0;
-                    break;
-                }
-            }
-
-            max = 0;
-            for (auto [ch, occ] : occurences_comp)
-            {
-                if (ch == 'J')
+                    num_j = occ;
                     continue;
-                max = std::max(occ, max);
-            }
-
-            for (auto [ch, occ] : occurences_comp)
-            {
-                if (ch == 'J')
-                    continue;
-                if (occ == max)
-                {
-                    occurences_comp[ch] += occurences_comp['J'];
-                    occurences_comp['J'] = 0;
-                    break;
                 }
+                occs.push_back(occ);
             }
 
-            auto rank = rankLevel(occurences);
-            auto rank_comp = rankLevel(occurences_comp);
+            std::sort(occs.rbegin(), occs.rend());
 
-            if (rank == rank_comp)
+            if (num_j == 5)
             {
-                for (int index = 0; index < cards.size(); ++index)
-                {
-                    if (cards[index] != comp.cards[index])
-                    {
-                        return cardStrength[cards[index]] > cardStrength[comp.cards[index]];
-                    }
-                }
-
-                return false;
+                occs.push_back(0);
             }
 
-            return rank > rank_comp;
+            occs[0] += num_j;
+
+            if (occs == std::vector<int8_t>{5})
+                return 7;
+
+            if (occs == std::vector<int8_t>{4, 1})
+                return 6;
+
+            if (occs == std::vector<int8_t>{3, 2})
+                return 5;
+
+            if (occs == std::vector<int8_t>{3, 1, 1})
+                return 4;
+
+            if (occs == std::vector<int8_t>{2, 2, 1})
+                return 3;
+
+            if (occs == std::vector<int8_t>{2, 1, 1, 1})
+                return 2;
+
+            if (occs == std::vector<int8_t>{1, 1, 1, 1, 1})
+                return 1;
+
+            return 0;
         }
     };
 
     std::vector<CamelCards> allCards;
 
+    void sortArray()
+    {
+        std::sort(allCards.begin(), allCards.end(), [=](const auto& a, const auto& b)
+            {
+                if (a.rank != b.rank)
+                    return a.rank < b.rank;
+                else
+                {
+                    for (int index = 0; index < a.cards.size(); ++index)
+                    {
+                        if (a.cards[index] != b.cards[index])
+                        {
+                            return cardStrength[a.cards[index]] < cardStrength[b.cards[index]];
+                        }
+                    }
+                }
+
+                return false;
+            });
+    }
+
     int64_t part1()
     {
+        cardStrength['2'] = 0;
+        cardStrength['3'] = 1;
+        cardStrength['4'] = 2;
+        cardStrength['5'] = 3;
+        cardStrength['6'] = 4;
+        cardStrength['7'] = 5;
+        cardStrength['8'] = 6;
+        cardStrength['9'] = 7;
+        cardStrength['T'] = 8;
+        cardStrength['J'] = 9;
+        cardStrength['Q'] = 10;
+        cardStrength['K'] = 11;
+        cardStrength['A'] = 12;
+
         for (auto line : inputVector)
         {
             auto card = util::split(line, ' ');
             CamelCards temp;
             temp.cards = card[0];
             temp.bid = std::stoi(card[1]);
+            temp.rank = temp.rankLevel(false);
             allCards.push_back(temp);
         }
 
-        bool shuffled = true;
-        while (shuffled)
-        {
-            shuffled = false;
-            for (int index = 0; index < allCards.size() - 1; ++index)
-            {
-                if (allCards[index].bigger(allCards[index + 1]))
-                {
-                    std::swap(allCards[index], allCards[index + 1]);
-                    shuffled = true;
-                }
-            }
-        }
+        sortArray();
 
         int64_t result = 0;
         for (int index = 0; index < allCards.size(); ++index)
-        {
             result += (index + 1) * allCards[index].bid;
-        }
 
         return result;
     }
 
     int64_t part2()
     {
-        bool shuffled = true;
-        while (shuffled)
-        {
-            shuffled = false;
-            for (int index = 0; index < allCards.size() - 1; ++index)
-            {
-                if (allCards[index].bigger_part2(allCards[index + 1]))
-                {
-                    std::swap(allCards[index], allCards[index + 1]);
-                    shuffled = true;
-                }
-            }
-        }
+        cardStrength['J'] = -1;
+
+        for (auto& card : allCards)
+            card.rank = card.rankLevel(true);
+
+        sortArray();
 
         int64_t result = 0;
         for (int index = 0; index < allCards.size(); ++index)
-        {
             result += (index + 1) * allCards[index].bid;
-        }
 
         return result;
     }
