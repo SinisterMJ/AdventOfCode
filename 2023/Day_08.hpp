728963fde8ed0d41abe9ae98d716b7ea95694b58
Anton Roth <Anton.Roth@corpuls.com>
2023-12-08 06:42:52 +0100

    Day 8

--- 2023/Day_08.hpp
+++ 2023/Day_08.hpp
@@ -10,14 +10,86 @@ private:
     std::vector<std::string> inputVector;
     std::string inputString;
 
+    std::map<std::string, std::pair<std::string, std::string>> elements;
+
     int64_t part1()
     {
-        return 0;
+        std::string instructions = inputVector[0];
+        for (auto line : inputVector)
+        {
+            if (line.find(" = ") == std::string::npos)
+                continue;
+
+            auto el = line.substr(0, 3);
+            auto el_l = line.substr(7, 3);
+            auto el_r = line.substr(12, 3);
+
+            elements[el] = std::make_pair(el_l, el_r);
+        }
+
+        std::string curr = "AAA";
+
+        int64_t steps = 0;
+        int ins = 0;
+        
+        while (curr != "ZZZ")
+        {
+            if (instructions[ins] == 'L')
+                curr = elements[curr].first;
+            if (instructions[ins] == 'R')
+                curr = elements[curr].second;
+            
+            ++ins;
+            ++steps;
+            if (ins == instructions.size())
+                ins = 0;
+        }
+
+        return steps;
     }
 
     int64_t part2()
     {
-        return 0;
+        std::vector<std::string> starts;
+        for (auto [node, proc] : elements)
+        {
+            if (node[2] == 'A')
+                starts.push_back(node);
+        }
+        std::string instructions = inputVector[0];
+
+
+        std::vector<int64_t> steps_ghosts;
+        
+        for (auto curr : starts)
+        {
+            int64_t steps = 0;
+            int ins = 0;
+
+            while (curr[2] != 'Z')
+            {
+                if (instructions[ins] == 'L')
+                    curr = elements[curr].first;
+                if (instructions[ins] == 'R')
+                    curr = elements[curr].second;
+
+                ++ins;
+                ++steps;
+                if (ins == instructions.size())
+                    ins = 0;
+            }
+
+            steps_ghosts.push_back(steps);
+        }
+
+        int64_t result = 1;
+
+        for (auto st : steps_ghosts)
+        {
+            result = lcm(result, st);
+        }
+
+        return result;
     }
 
 public:
