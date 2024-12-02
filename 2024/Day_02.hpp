b2597747d5a8e0e42b8adafcfcde7c1c11439eb0
Anton Roth <Anton.Roth@corpuls.com>
2024-12-02 08:43:33 +0100

    Day 2 solved.

--- 2024/Day_02.hpp
+++ 2024/Day_02.hpp
@@ -12,12 +12,58 @@ private:
 
     int64_t part1()
     {
-        return 0;
+        int save = 0;
+        for (auto line : inputVector)
+        {
+            auto values = util::splitInt(line, ' ');
+            bool line_save = true;
+            bool ascending = values[0] < values[1];
+            for (int i = 1; i < values.size(); ++i)
+            {
+                if (!in_range(std::abs(values[i] - values[i - 1]), 1, 3))
+                    line_save = false;
+
+                if ((values[i - 1] < values[i]) != ascending)
+                    line_save = false;
+            }
+
+            save += line_save;
+        }
+        return save;
     }
 
     int64_t part2()
     {
-        return 0;
+        int save = 0;
+        for (auto line : inputVector)
+        {
+            auto values = util::splitInt(line, ' ');
+
+            bool fix_found = false;
+            for (int index = 0; index < values.size(); ++index)
+            {
+                auto copy = values;
+                copy.erase(copy.begin() + index);
+
+                bool line_save = true;
+                bool ascending = copy[0] < copy[1];
+                for (int i = 1; i < copy.size(); ++i)
+                {
+                    if (!in_range(std::abs(copy[i] - copy[i - 1]), 1, 3))
+                        line_save = false;
+
+                    if ((copy[i - 1] < copy[i]) != ascending)
+                        line_save = false;
+                }
+
+                fix_found |= line_save;
+                if (fix_found)
+                    break;
+            }            
+
+            save += fix_found;
+        }
+        return save;
     }
 
 public:
