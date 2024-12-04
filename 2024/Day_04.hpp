72a05f1327968060a2e679ac27bec43672e5c7ba
Anton Roth <Anton.Roth@corpuls.com>
2024-12-04 22:13:33 +0100

    Day 4 solve

--- 2024/Day_04.hpp
+++ 2024/Day_04.hpp
@@ -3,6 +3,8 @@
 
 #include "../includes/aoc.h"
 #include <algorithm>
+#include "../includes/Map2DBase.h"
+
 
 class Day04 {
 private:
@@ -10,14 +12,45 @@ private:
     std::vector<std::string> inputVector;
     std::string inputString;
 
+    Map2DBase<uint8_t> xmas_map;
+
     int64_t part1()
     {
-        return 0;
+        int result = 0;
+
+        for (int y = 0; y < inputVector.size(); ++y)
+            for (int x = 0; x < inputVector[y].size(); ++x)
+                xmas_map.write(x, y, inputVector[y][x]);
+
+        auto directions = MapHelper::getNeighboursVec(true);
+
+        for (auto all_x : xmas_map.findAll('X'))
+            for (auto direction : directions)
+                result += (xmas_map.read(all_x + direction) == 'M') && (xmas_map.read(all_x + direction * 2) == 'A') && (xmas_map.read(all_x + direction * 3) == 'S');
+
+        return result;
     }
 
     int64_t part2()
     {
-        return 0;
+        int result = 0;
+        for (auto all_a : xmas_map.findAll('A'))
+        {
+            std::string left = "";
+            left += xmas_map.read(all_a + v2(-1, -1));
+            left += xmas_map.read(all_a + v2(1, 1));
+
+            std::string right = "";
+            right += xmas_map.read(all_a + v2(-1, 1));
+            right += xmas_map.read(all_a + v2(1, -1));
+
+            std::sort(left.begin(), left.end());
+            std::sort(right.begin(), right.end());
+
+            result += (left == "MS" && right == "MS");
+        }
+
+        return result;
     }
 
 public:
