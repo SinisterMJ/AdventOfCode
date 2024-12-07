8476ed2c92bd4ebb071ab77a0b658adacc65e2ac
SinisterMJ <Anton_Roth@gmx.de>
2024-12-07 09:05:55 +0100

    Day 7 solve

--- 2024/Day_07.hpp
+++ 2024/Day_07.hpp
@@ -3,21 +3,116 @@
 
 #include "../includes/aoc.h"
 #include <algorithm>
+#include <bitset>
 
 class Day07 {
 private:
 
     std::vector<std::string> inputVector;
     std::string inputString;
-
+    
     int64_t part1()
     {
-        return 0;
+        std::map<int64_t, std::vector<int64_t>> equations;
+        int64_t sum = 0;
+        for (auto line : inputVector)
+        {
+            auto els = util::split(line, ':');
+            int64_t left = std::stoll(els[0]);
+            auto right = util::splitInt64(els[1], ' ');
+
+            equations[left] = right;
+        }
+
+        for (auto [left, right] : equations)
+        {
+            for (int i = 0; i < (0x1 << (right.size() - 1)); ++i)
+            {
+                auto binary = std::bitset<16>(i);
+                int64_t total = right[0];
+                for (int j = 1; j < right.size(); ++j)
+                {
+                    if (binary[j - 1])
+                        total += right[j];
+                    else
+                        total *= right[j];
+                }
+
+                if (total == left)
+                {
+                    sum += total;
+                    break;
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    std::string intToTernary(int64_t input)
+    {
+        std::string result = "";
+
+        if (input == 0)
+            return "0";
+
+        while (input != 0)
+        {
+            auto rest = input % 3;
+            result = std::to_string(rest) + result;
+            input /= 3;
+        }
+
+        return result;
     }
 
     int64_t part2()
     {
-        return 0;
+        std::map<int64_t, std::vector<int64_t>> equations;
+        int64_t sum = 0;
+        for (auto line : inputVector)
+        {
+            auto els = util::split(line, ':');
+            int64_t left = std::stoll(els[0]);
+            auto right = util::splitInt64(els[1], ' ');
+
+            equations[left] = right;
+        }
+
+        for (auto [left, right] : equations)
+        {
+            for (int i = 0; i < std::pow(3, right.size() - 1); ++i)
+            {
+                std::string versions = intToTernary(i);
+
+                while (versions.size() < (right.size() - 1))
+                    versions = "0" + versions;
+
+                int64_t total = right[0];
+                for (int j = 1; j < right.size(); ++j)
+                {
+                    if (versions[j - 1] == '0')
+                        total += right[j];
+                    if (versions[j - 1] == '1')
+                        total *= right[j];
+                    if (versions[j - 1] == '2')
+                    {
+                        std::string l = std::to_string(total);
+                        std::string r = std::to_string(right[j]);
+                        auto lr = l + r;
+                        total = std::stoll(lr);
+                    }
+                }
+
+                if (total == left)
+                {
+                    sum += total;
+                    break;
+                }
+            }
+        }
+
+        return sum;
     }
 
 public:
