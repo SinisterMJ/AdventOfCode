91fd9ed402c5e01450ca782a3c7b35499bc68a66
SinisterMJ <Anton_Roth@gmx.de>
2024-12-07 09:13:36 +0100

    Updated

--- 2023/Day_25.hpp
+++ 2023/Day_25.hpp
@@ -10,8 +10,92 @@ private:
     std::vector<std::string> inputVector;
     std::string inputString;
 
+    std::map<std::string, std::vector<std::string>> connections;
+
+    int32_t getNumGroup(std::string node, std::map<std::string, std::vector<std::string>> connections_altered)
+    {
+        std::set<std::string> localNodes;
+        localNodes.insert(node);
+
+        bool added = true;
+
+        while (added)
+        {
+            added = false;
+
+            for (auto n : localNodes)
+            {
+                auto targets = connections_altered[n];
+                for (auto t : targets)
+                {
+                    if (!localNodes.contains(t))
+                    {
+                        added = true;
+                        localNodes.insert(t);
+                    }
+                }
+            }
+        }
+
+        return localNodes.size();
+    }
+
     int64_t part1()
     {
+        std::set<std::string> comp;
+
+        for (auto line : inputVector)
+        {
+            auto parts = util::split(line, ": ");
+            auto subparts = util::split(parts[1], ' ');
+
+            comp.insert(parts[0]);
+
+            for (auto part : subparts)
+            {
+                connections[part].push_back(parts[0]);
+                connections[parts[0]].push_back(part);
+                comp.insert(part);
+            }
+        }
+
+        std::vector<std::string> components(comp.begin(), comp.end());
+
+        for (int i = 0; i < components.size(); ++i)
+        {
+            auto comp_i = components[i];
+            for (int j = i + 1; j < components.size(); ++j)
+            {
+                auto comp_j = components[j];
+                for (int k = j + 1; k < components.size(); ++k)
+                {
+                    auto comp_k = components[k];
+                    for (int i_i = 0; i_i < connections[comp_i].size(); i_i++)
+                    {
+                        for (int j_i = 0; j_i < connections[comp_j].size(); j_i++)
+                        {
+                            for (int k_i = 0; k_i < connections[comp_k].size(); k_i++)
+                            {
+                                auto altered = connections;
+                                altered[comp_i].erase(altered[comp_i].begin() + i_i);
+                                altered[comp_j].erase(altered[comp_j].begin() + j_i);
+                                altered[comp_k].erase(altered[comp_k].begin() + k_i);
+
+                                int32_t groupSize = getNumGroup(components[0], altered);
+
+                                if (groupSize != components.size())
+                                {
+                                    return (components.size() - groupSize) * groupSize;
+                                }
+
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
         return 0;
     }
 
--- GetData.py
+++ GetData.py
@@ -7,23 +7,26 @@ from pathlib import Path
 day = datetime.datetime.now().day
 year = datetime.datetime.now().year
 
-if len(sys.argv) >= 2:
-	year = sys.argv[1]
-	
-if len(sys.argv) == 3:
-	day = sys.argv[2]
+start_year = 2015
+max_day = 25
 
-fileName = r"./inputs/" + str(year) + "/input_" + str(day) + ".txt"
-filePath = r"./inputs/" + str(year) + "/"
 
-Path(filePath).mkdir(parents=True, exist_ok=True)
+for y in range(start_year, year + 1):
+    for d in range(1, max_day + 1):
+        if d > day and y == year:
+            continue
 
-if os.path.isfile(fileName):
-    print ("File exist. No request should be done.")
-	
-cookies = dict(session='53616c7465645f5fb39dce8967cd5f0d658dc39f2f1e1c303b16864af0d7ccc501f191948b416e25e68cc0dad5f9606a273ad47b8c3228b968e3389cb8bf4208')
-r = requests.get("https://adventofcode.com/" + str(year) + "/day/" + str(day) + "/input", cookies=cookies)
-file = open(fileName, "w") 
-file.write(r.text)
-file.close()
-print(r.text)
+        fileName = r"./inputs/" + str(y) + "/input_" + str(d) + ".txt"
+        filePath = r"./inputs/" + str(y) + "/"
+
+        Path(filePath).mkdir(parents=True, exist_ok=True)
+
+        if os.path.isfile(fileName):
+            print ("File exist. No request should be done.")
+            
+        cookies = dict(session='53616c7465645f5fb39dce8967cd5f0d658dc39f2f1e1c303b16864af0d7ccc501f191948b416e25e68cc0dad5f9606a273ad47b8c3228b968e3389cb8bf4208')
+        r = requests.get("https://adventofcode.com/" + str(y) + "/day/" + str(d) + "/input", cookies=cookies)
+        file = open(fileName, "w") 
+        file.write(r.text)
+        file.close()
+        print(r.text)
