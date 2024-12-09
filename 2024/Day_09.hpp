2874657990b926a080dee22bea62a0ee5876040d
Anton Roth <Anton.Roth@corpuls.com>
2024-12-09 07:54:09 +0100

    Day 9 solution. That took longer than needed - reason is the middle files are touching, and code did not properly detect this

--- 2024/Day_09.hpp
+++ 2024/Day_09.hpp
@@ -12,12 +12,123 @@ private:
 
     int64_t part1()
     {
-        return 0;
+        std::vector<int32_t> memory;
+        int counter = 0;
+        
+        for (auto ch : inputVector[0])
+        {
+            int value = counter / 2;
+            if (counter % 2 == 1)
+                value = -1;
+
+            for (int i = 0; i < (ch - '0'); ++i)
+                memory.push_back(value);
+
+            counter++;
+        }
+
+        int last = static_cast<int>(memory.size() - 1);
+        int first = 0;
+
+        for (;;)
+        {
+            while (memory[last] == -1)
+                last--;
+
+            while (memory[first] != -1)
+                first++;
+
+            if (first >= last)
+                break;
+
+            std::swap(memory[first], memory[last]);
+        }
+
+        int64_t result = 0;
+
+        for (int index = 0; index < memory.size(); ++index)
+        {
+            if (memory[index] != -1)
+                result += index * memory[index];
+        }
+
+        return result;
     }
 
     int64_t part2()
     {
-        return 0;
+        std::vector<int32_t> memory;
+        int counter = 0;
+
+        for (auto ch : inputVector[0])
+        {
+            int value = counter / 2;
+            if (counter % 2 == 1)
+                value = -1;
+
+            for (int i = 0; i < (ch - '0'); ++i)
+                memory.push_back(value);
+
+            counter++;
+        }
+
+        int last = static_cast<int>(memory.size() - 1);
+        int first = 0;
+
+        for (;;)
+        {
+            while (memory[last] == -1)
+                last--;
+
+            if (last < 0)
+                break;
+
+            int end = last;
+            int value = memory[last];
+            int count = 0;
+            while (last >= 0 && memory[last] == value)
+            {
+                last--;
+                count++;
+            }
+
+            first = 0;
+            while (true)
+            {
+                while (memory[first] != -1)
+                    ++first;
+
+                int start = first;
+                int place = 0;
+                while (first < memory.size() && memory[first] == -1)
+                {
+                    first++;
+                    place++;
+                }
+
+                if (start < end and place >= count)
+                {
+                    for (int i = 0; i < count; ++i)
+                        std::swap(memory[start + i], memory[end - i]);
+
+                    break;
+                }
+
+                if (first >= last)
+                    break;
+            }
+            
+            if (last <= 0)
+                break;
+        }
+
+        int64_t result = 0;
+
+        for (int index = 0; index < memory.size(); ++index)
+            if (memory[index] != -1)
+                result += index * memory[index];
+
+        return result;
     }
 
 public:
