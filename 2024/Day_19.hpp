195c80873c73d98c209f077e51b8cccb958c9b08
SinisterMJ <Anton_Roth@gmx.de>
2024-12-22 23:18:56 +0100

    Day 19 solved

--- 2024/Day_19.hpp
+++ 2024/Day_19.hpp
@@ -12,12 +12,74 @@ private:
 
     int64_t part1()
     {
-        return 0;
+        auto hats = util::split(inputVector[0], ", ");
+        std::set<std::string> patterns(hats.begin(), hats.end());
+
+        std::vector<std::string> targets;
+        std::copy(inputVector.begin() + 2, inputVector.end(), std::back_inserter(targets));
+
+        int result = 0;
+        for (auto target : targets)
+        {
+            std::set<int32_t> seen_offsets;
+            std::queue<int32_t> offsets;
+            offsets.push(0);
+            seen_offsets.insert(0);
+
+            while (!offsets.empty())
+            {
+                int32_t off = offsets.front(); offsets.pop();
+                if (off == target.size())
+                {
+                    result += 1;
+                    break;
+                }
+
+                for (auto pattern : patterns)
+                {
+                    if (target.substr(off, pattern.size()) == pattern && !seen_offsets.contains(off + pattern.size()))
+                    {
+                        offsets.push(off + pattern.size());
+                        seen_offsets.insert(off + pattern.size());
+                    }
+                }
+            }
+        }
+
+        return result;
     }
 
     int64_t part2()
     {
-        return 0;
+
+        auto hats = util::split(inputVector[0], ", ");
+        std::set<std::string> patterns(hats.begin(), hats.end());
+
+        std::vector<std::string> targets;
+        std::copy(inputVector.begin() + 2, inputVector.end(), std::back_inserter(targets));
+
+        int64_t result = 0;
+        for (auto target : targets)
+        {
+            std::map<int32_t, int64_t> seen_offsets;
+            seen_offsets[0] = 1;
+
+            for (int index = 1; index <= target.size(); ++index)
+            {                
+                for (auto pattern : patterns)
+                {
+                    if (index - static_cast<int32_t>(pattern.size()) < 0)
+                        continue;
+
+                    if (target.substr(index - pattern.size(), pattern.size()) == pattern)
+                        seen_offsets[index] += seen_offsets[index - pattern.size()];
+                }
+            }
+
+            result += seen_offsets[target.size()];
+        }
+
+        return result;
     }
 
 public:
