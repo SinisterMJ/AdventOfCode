37fdec61b3374a3c03582370263c2b388e394a4c
Anton Roth <Anton.Roth@corpuls.com>
2025-12-01 09:06:21 +0100

    Day 1 2025

--- 2025/Day_01.hpp
+++ 2025/Day_01.hpp
@@ -12,12 +12,48 @@ private:
 
     int64_t part1()
     {
-        return 0;
+        int start = 50;
+        int zeros = 0;
+        for (auto turn : inputVector)
+        {
+            int sign = turn[0] == 'L' ? -1 : 1;
+            int count = std::stoi(turn.substr(1));
+            start += sign * count;
+            start = (start % 100 + 100) % 100;
+
+            if (start == 0)
+            {
+                zeros++;
+            }
+        }
+        return zeros;
     }
 
     int64_t part2()
     {
-        return 0;
+        int start = 50;
+        int zeros = 0;
+        int lineCount = 0;
+        bool started_zero = false;
+        for (auto turn : inputVector)
+        {
+            started_zero = false;
+            if (start == 0)
+                started_zero = true;
+
+            int sign = turn[0] == 'L' ? -1 : 1;
+            int count = std::stoi(turn.substr(1));
+            start += sign * count;
+
+            if (start >= 100)
+                zeros += start / 100;
+
+            if (start <= 0)
+                zeros += -start / 100 + 1 - started_zero;
+
+            start = (start % 100 + 100) % 100;
+        }
+        return zeros;
     }
 
 public:
