1de02163660a503e2dfe493be245edb98cd07319
Anton Roth <Anton.Roth@corpuls.com>
2025-12-03 07:08:03 +0100

    Optimized readability

--- 2025/Day_03.hpp
+++ 2025/Day_03.hpp
@@ -8,38 +8,22 @@ class Day03 {
 private:
 
     std::vector<std::string> inputVector;
-    std::string inputString;
 
-    int64_t part1()
-    {
-        int sum = 0;
-        for (const std::string& line : inputVector)
-        {
-            auto first = std::max_element(line.begin(), line.end() - 1);
-            auto second = std::max_element(first + 1, line.end());
-            sum += (*first - '0') * 10 + (*second - '0');
-        }
-
-        return sum;
-    }
-
-    int64_t part2()
+    int64_t run(const int batteries_on)
     {
         int64_t sum = 0;
         for (const std::string& line : inputVector)
         {
             int64_t joltage = 0;
             auto start = line.begin();
-            for (int i = 0; i < 12; ++i)
+            for (int i = 0; i < batteries_on; ++i)
             {
-                auto val = std::max_element(start, line.end() - 11 + i);
+                auto val = std::max_element(start, line.end() - batteries_on + 1 + i);
                 joltage = joltage * 10 + (*val - '0');
                 start = val + 1;
             }
-
             sum += joltage;
         }
-
         return sum;
     }
 
@@ -54,8 +38,8 @@ public:
         util::Timer myTime;
         myTime.start();
 
-        auto result_1 = part1();
-        auto result_2 = part2();
+        auto result_1 = run(2);
+        auto result_2 = run(12);
 
         int64_t time = myTime.usPassed();
 
