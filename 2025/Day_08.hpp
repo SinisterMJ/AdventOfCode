771a393f2a2e95e2e9880e799d5ca8678f5dec17
Anton Roth <Anton.Roth@corpuls.com>
2025-12-08 10:21:01 +0100

    Fixed some code

--- 2025/Day_08.hpp
+++ 2025/Day_08.hpp
@@ -9,79 +9,10 @@ private:
 
     std::vector<std::string> inputVector;
 
-    int64_t part1()
+    std::pair<int64_t, int64_t> run_parts()
     {
         std::vector<v3> boxes;
-
-        for (const std::string& line : inputVector)
-        {
-            auto coords = util::splitInt64(line, ',');
-            boxes.push_back(v3(coords[0], coords[1], coords[2]));
-        }
-
-        std::map<std::pair<int, int>, float> distances;
-        std::map<int, int> circuit_map;
-        std::vector<float> dist_vec;
-        int curr_circuit = 0;
-
-        for (int i = 0; i < boxes.size(); ++i)
-        {
-            circuit_map[i] = i;
-            for (int j = i + 1; j < boxes.size(); ++j)
-            {
-                v3 diff = boxes[i] - boxes[j];
-                float manh = diff.length();
-
-                distances[{i, j}] = manh;
-                dist_vec.push_back(manh);
-            }
-        }
-
-        std::sort(dist_vec.begin(), dist_vec.end());
-        std::vector<std::set<int>> set_circuits;
-
-        for (auto& junc_pair : distances)
-            for (int i = 0; i < 1000; ++i)
-                if (dist_vec[i] == junc_pair.second)
-                    set_circuits.push_back({ junc_pair.first.first, junc_pair.first.second });
-
-        bool merged = true;
-        while (merged)
-        {
-            merged = false;
-            for (int i = 0; i < set_circuits.size(); ++i)
-            {
-                for (int j = i + 1; j < set_circuits.size(); ++j)
-                {
-                    std::set<int> intersection;
-                    std::set_intersection(set_circuits[i].begin(), set_circuits[i].end(),
-                                          set_circuits[j].begin(), set_circuits[j].end(),
-                                          std::inserter(intersection, intersection.begin()));
-                    if (!intersection.empty())
-                    {
-                        set_circuits[i].insert(set_circuits[j].begin(), set_circuits[j].end());
-                        set_circuits.erase(set_circuits.begin() + j);
-                        merged = true;
-                        break;
-                    }
-                }
-                if (merged)
-                    break;
-            }
-        }
-
-        std::vector<int> counts;
-        for (const auto& sets : set_circuits)
-            counts.push_back(sets.size());
-
-        std::sort(counts.begin(), counts.end(), std::greater<int>());       
-
-        return counts[0] * counts[1] * counts[2];
-    }
-
-    int64_t part2()
-    {
-        std::vector<v3> boxes;
+        int64_t part1_result = 0;
 
         for (const std::string& line : inputVector)
         {
@@ -108,6 +39,15 @@ private:
         int index = 0;
         while (subsets[0].size() != boxes.size())
         {
+            if (index == 1000)
+            {
+                std::vector<int> counts;
+                for (const auto& sets : subsets)
+                    counts.push_back(sets.size());
+
+                std::sort(counts.begin(), counts.end(), std::greater<int>());
+                part1_result = counts[0] * counts[1] * counts[2];
+            }
             auto index_l = distances[index].second.first;
             auto index_r = distances[index].second.second;
 
@@ -149,7 +89,7 @@ private:
             ++index;
         }
 
-        return boxes[distances[index - 1].second.first].x * boxes[distances[index - 1].second.second].x;
+        return std::make_pair(part1_result, boxes[distances[index - 1].second.first].x * boxes[distances[index - 1].second.second].x);
     }
 
 public:
@@ -163,8 +103,9 @@ public:
         util::Timer myTime;
         myTime.start();
 
-        auto result_1 = part1();
-        auto result_2 = part2();
+        auto result = run_parts();
+        auto result_1 = result.first;
+        auto result_2 = result.second;
 
         int64_t time = myTime.usPassed();
 
